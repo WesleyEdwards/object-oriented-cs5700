1a37c09e07cdb7335d5684d101779c0bc5c0e48b
WesleyEdwards <westheedwards@gmail.com>
2023-04-01 09:05:00 -0600

    Naked double working

--- sudoku-solver/src/lib/solvingTechniques.ts/NakedDouble.ts
+++ sudoku-solver/src/lib/solvingTechniques.ts/NakedDouble.ts
@@ -19,24 +19,22 @@ export class NakedDouble implements SolveMethodTemplate {
     // find in box all values in the same row or column.
     // NakedPair: Two candidates are same in two cells in a row, column or box
     const boxes: Cell[][] = getGridBoxes(grid);
+
     const doubleRows: DoubleRowInfo[] = this.findDoubleRowInfo(boxes);
-    // console.log(doubleRows);
-    doubleRows.forEach((doubleRow) => {
-      const otherBoxesInRow = boxes.filter((boxCells) => {
-        return (
-          boxCells.every((c) => c.col !== doubleRow.col1) &&
-          boxCells.some((c) => c.row === doubleRow.row)
-        );
-      });
-      otherBoxesInRow.forEach((box1) => {
-        const rowCells = box1.filter((c) => c.row === doubleRow.row);
-        rowCells.forEach((c) => {
-          if (c.assignedValue) return;
-          if (c.possibleValues.length === 1) {
-            c.assignedValue = c.possibleValues[0];
-          }
-          c.possibleValues = c.possibleValues.filter(
-            (value) => value !== doubleRow.value
+    console.log(doubleRows);
+    grid.forEach((row) => {
+      row.forEach((cell) => {
+        if (cell.assignedValue) return;
+        if (cell.possibleValues.length !== 2) return;
+        doubleRows.forEach((info) => {
+          if (info.cell1.row !== cell.row) return;
+          if (info.cell1.col === cell.col) return;
+          if (info.cell2.col === cell.col) return;
+          if (cell.possibleValues.length === 1)
+            cell.assignedValue = cell.possibleValues[0];
+          console.log(info.cell1, info.cell2, cell);
+          cell.possibleValues = cell.possibleValues.filter(
+            (v) => v !== info.value
           );
         });
       });
@@ -58,36 +56,41 @@ export class NakedDouble implements SolveMethodTemplate {
 
   findDoubleRowInfo(boxes: Cell[][]): DoubleRowInfo[] {
     const doubleRows: DoubleRowInfo[] = [];
-    for (let i = 0; i < boxes.length; i++) {
-      const boxCells = boxes[i];
-      const rowIndexes = [...new Set(boxCells.map((cell) => cell.row))];
-      rowIndexes.forEach((row) => {
-        const rowCells = boxCells.filter((cell) => cell.row === row);
-        const rowValues = rowCells.map((cell) => cell.possibleValues).flat();
-        const uniqueValuesInRow = [...new Set(rowValues)];
-        const otherValuesInBox = boxCells
-          .filter((cell) => cell.row !== row)
-          .map((cell) => cell.possibleValues)
-          .flat();
-        const doubleValues = uniqueValuesInRow.filter(
-          (value) => !otherValuesInBox.includes(value)
-        );
-        doubleValues.forEach((value) => {
-          doubleRows.push({
-            row,
-            value,
-            col1: rowCells[0].col,
-            col2: rowCells.length > 1 ? rowCells[1].col : rowCells[0].col,
-          });
+    boxes.forEach((box) => {
+      const possibleDoubles: Cell[] = [];
+      box.forEach((c) => {
+        if (c.assignedValue) return;
+        if (c.possibleValues.length !== 2) return;
+        const [value1, value2] = c.possibleValues;
+        const rowCells = box.filter((cell) => cell.row === c.row);
+        const rowCellsWithSameValue = rowCells.filter((cell) => {
+          if (cell.assignedValue) return false;
+          if (cell.possibleValues.length !== 2) return false;
+          return (
+            cell.possibleValues.includes(value1) &&
+            cell.possibleValues.includes(value2)
+          );
         });
+        if (rowCellsWithSameValue.length > 1) {
+          const other1 = rowCellsWithSameValue[0];
+          const otherCell = rowCellsWithSameValue[1];
+          doubleRows.push({
+            row: c.row,
+            cell1: other1,
+            cell2: otherCell,
+            value: value1,
+            value2: value2,
+          });
+        }
       });
-    }
+    });
     return doubleRows;
   }
 }
 type DoubleRowInfo = {
   row: number;
-  col1: number;
-  col2: number;
+  cell1: Cell;
+  cell2: Cell;
   value: string;
+  value2: string;
 };
